25c00506703ad8915a45b106a08e97888def4b39
alreadyDev <alreadydev24@gmail.com>
2025-05-09 12:28:01 -0400

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+#include "main.h"
+
+BLECharacteristic *encoderDataCharacteristic;
+int counter = 0;
+String encoderData;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Starting BLE work!");
+ 
+  BLEDevice::init("encoder-sensor");  //creamos el dispositivo
+
+  BLEServer *pServer = BLEDevice::createServer(); // creamos server
+  BLEService *pService = pServer->createService(SERVICE_UUID); // creamos servicio
+
+  encoderDataCharacteristic = pService->createCharacteristic(
+    ENCODER_CHARACTERISTIC_UUID,
+    BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY);
+  encoderDataCharacteristic->addDescriptor(new BLE2902());
+
+  pService->start();
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06);
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+  
+  #ifdef DEBUG
+  Serial.println("BLE DEVICE READY TO BE CONNECTED");
+  #endif
+
+}
+
+void loop(){
+  encoderData = String(counter);
+  encoderDataCharacteristic->setValue(encoderData.c_str());
+  encoderDataCharacteristic->notify();
+  counter++;
+  delay(2000);
+
+}
---
+++ src/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <ArduinoJson.h>
+#include <Arduino.h>
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <BLE2902.h>
+
+#define SERVICE_UUID "dc3048cc-4347-4256-8a06-6f0af67f2132"
+#define CHARACTERISTIC_UUID "099a8591-df27-4f7d-8634-233c5b5b5075"
+#define ENCODER_CHARACTERISTIC_UUID "08a90be8-81a3-4527-911d-38162f772296"
+#define DEBUG
+
+#endif
